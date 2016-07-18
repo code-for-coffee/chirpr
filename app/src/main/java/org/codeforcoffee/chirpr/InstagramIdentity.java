e6d54c704c3c78881906853353414886192c4154
code-for-coffee <jtraverjr@gmail.com>
2016-07-17 21:50:23 -0500

    identity

--- app/src/main/java/org/codeforcoffee/chirpr/InstagramIdentity.java
+++ app/src/main/java/org/codeforcoffee/chirpr/InstagramIdentity.java
@@ -4,5 +4,8 @@ package org.codeforcoffee.chirpr;
  * Created by codeforcoffee on 7/17/16.
  */
 public class InstagramIdentity {
-
+    // Instagram Client Name: AndroidOAuthLesson
+    public static final String CLIENT_ID = "4a32b992d90643e181b09276630c15dc";
+    public static final String CLIENT_SECRET = "ce4d43ddded94d56a8d3a8ebc8562a1f";
+    public static final String CALLBACK_URL = "http://generalassemb.ly";
 }
