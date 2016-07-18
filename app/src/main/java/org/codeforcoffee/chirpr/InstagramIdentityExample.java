ce4100da1e3b30348bde430aa361ea2b0cf8377e
code-for-coffee <jtraverjr@gmail.com>
2016-07-17 21:44:35 -0500

    gitignore + example

---
+++ app/src/main/java/org/codeforcoffee/chirpr/InstagramIdentityExample.java
@@ -0,0 +1,12 @@
+package org.codeforcoffee.chirpr;
+
+/**
+ * Created by codeforcoffee on 7/17/16.
+ * Refactor/rename this file to InstagramIdentity.java;
+ * Doing so will keep it hidden via .gitignore
+ */
+public class InstagramIdentityExample {
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String CALLBACK_URL = "";
+}
