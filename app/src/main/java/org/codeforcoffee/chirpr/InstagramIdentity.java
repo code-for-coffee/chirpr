c4db5e2f865cb5b17ca747d4dabb712b51192606
code-for-coffee <jtraverjr@gmail.com>
2016-07-17 21:45:29 -0500

    identity class

---
+++ app/src/main/java/org/codeforcoffee/chirpr/InstagramIdentity.java
@@ -0,0 +1,10 @@
+package org.codeforcoffee.chirpr;
+
+/**
+ * Created by codeforcoffee on 7/17/16.
+ */
+public class InstagramIdentity {
+    public static final String CLIENT_ID = "";
+    public static final String CLIENT_SECRET = "";
+    public static final String CALLBACK_URL = "";
+}
